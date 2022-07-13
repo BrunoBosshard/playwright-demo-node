4c7fbd23ab2efffecc43f5ac2bcc7b1ec6edb08a
Bruno Bosshard <bruno.bosshard@pepgo.com>
2022-07-13 16:14:35 +1000

    Added Data-Driven Test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,3 +5,11 @@ test('basic test', async ({ page }) => {
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
 });
+
+// Data-Driven Test
+const urls = require('../data/urls.json');
+for (const url of urls) {
+  test(`check ${url}`, async ({ page }) => {
+    await page.goto(url);
+  })
+}
