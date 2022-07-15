414c2abc9b824a79c176683dcbbd0565ec940ff7
Bruno Bosshard <bruno.bosshard@pepgo.com>
2022-07-15 12:07:58 +1000

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,15 +1,19 @@
 const { test, expect } = require('@playwright/test');
 
-test('basic test', async ({ page }) => {
-  await page.goto('/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
-});
+test.describe('Example tests', () => {
 
-// Data-Driven Test
-const urls = require('../data/urls.json');
-for (const url of urls) {
-  test(`check ${url}`, async ({ page }) => {
-    await page.goto(url);
-  })
-}
+  test('basic test', async ({ page }) => {
+    await page.goto('/');
+    const title = page.locator('.navbar__inner .navbar__title');
+    await expect(title).toHaveText('Playwright');
+  });
+
+  // Data-Driven Test
+  const urls = require('../data/urls.json');
+  for (const url of urls) {
+    test(`check ${url}`, async ({ page }) => {
+      await page.goto(url);
+    });
+  };
+
+});
