9ca0765705135be6755b74b425af361ba7de640c
Bruno Bosshard <bruno.bosshard@pepgo.com>
2022-07-15 11:43:32 +1000

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('/');
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
 });
